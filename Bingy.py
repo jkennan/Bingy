f1dfe0df4106110aadbdd4cc7392aa4a9704fb1e
jkennan <jkennan97@gmail.com>
2017-03-22 21:03:49 -0400

    Hello world - opens browser to cnn.com

---
+++ Bingy.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+def bing_helper():
+    webbrowser.open('http://www.cnn.com')
+
+if __name__ == '__main__':
+    bing_helper()
---
+++ Bingy_Run.bat
@@ -0,0 +1,2 @@
+@echo off
+python "C:\Users\Jeffrey Kennan\Documents\GitHub\Bingy\Bingy.py" %*
