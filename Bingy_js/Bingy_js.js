600382efba3ad5f0177e890da0789e1ff06cbc06
jkennan <jkennan97@gmail.com>
2017-03-23 11:21:16 -0400

    Basically broke everything

---
+++ Bingy_js/Bingy_js.js
@@ -0,0 +1,25 @@
+alert('I got here!');
+main();
+
+function bing_helper(url) {
+  alert('Hello World');
+
+  window.location.href = url;
+}
+
+function bing_mobile(url) {
+  navigator.__defineGetter__('userAgent',
+  function() {
+    return 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'
+  })
+  window.location.href = url;
+  // bing_helper(url);
+}
+
+function main() {
+  bing_helper('http://www.cnn.com');
+  setTimeout(function() {alert('sleeping');}, 3000);
+//  bing_mobile('http://www.nytimes.com');
+  // setTimeout(bing_helper('http://www.cnn.com'), 3000);
+  // setTimeout(bing_mobile('http://www.nytimes.com'), 3000);
+}
--- Bingy_old/Bingy_js.js
+++ Bingy_old/Bingy_js.js
@@ -1,3 +1,6 @@
+main();
+
+
 function bing_helper(url) {
   console.log('Hello World');
 
@@ -9,13 +12,13 @@ function bing_mobile(url) {
   function() {
     return 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'
   })
-
-  bing_helper(url);
+  window.location.href = url;
+  // bing_helper(url);
 }
 
 function main() {
   bing_helper('http://www.cnn.com');
   bing_mobile('http://www.nytimes.com');
   // setTimeout(bing_helper('http://www.cnn.com'), 3000);
-  // setTimeout(bing_helper('http://www.nytimes.com'), 3000);
+  // setTimeout(bing_mobile('http://www.nytimes.com'), 3000);
 }
